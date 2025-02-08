9c7f39ee47e323eebf1b8ca5c19a78ff37c53f88
Sandesh <sandeshsubedi2020@gmail.com>
2025-02-08 22:32:42 +0545

    fix dependencies issue

--- app/(invite)/(routes)/invite/[inviteCode]/page.tsx
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -1,3 +1,65 @@
+// import { currentProfile } from '@/lib/current-profile'
+// import { db } from '@/lib/db'
+// import { redirect } from 'next/navigation'
+
+// interface inviteCodePageProps {
+//     params: {
+//         inviteCode: string
+//     }
+// }
+
+// const InviteCodePage = async ({
+//     params
+// }: inviteCodePageProps) => {
+//     const profile = await currentProfile()
+
+//     if (!profile) {
+//         return redirect('/sign-in')
+//     }
+
+//     if (!params.inviteCode) {
+//         return redirect('/')
+//     }
+
+//     const existingServer = await db.server.findFirst({
+//         where: {
+//             inviteCode: params.inviteCode,
+//             members: {
+//                 some: {
+//                     profileId: profile.id
+//                 }
+//             }
+//         }
+//     })
+
+//     if (existingServer) {
+//         return redirect(`/servers/${existingServer.id}`)
+//     }
+
+//     const server = await db.server.update({
+//         where: {
+//             inviteCode: params.inviteCode
+//         },
+//         data: {
+//             members: {
+//                 create: {
+//                     profileId: profile.id
+//                 }
+//             }
+//         }
+//     })
+
+//     if(server) {
+//         return redirect(`/servers/${server.id}`)
+//     }
+    
+//     return null
+// }
+
+// export default InviteCodePage
+
+
+
 import { currentProfile } from '@/lib/current-profile'
 import { db } from '@/lib/db'
 import { redirect } from 'next/navigation'
@@ -11,19 +73,20 @@ interface inviteCodePageProps {
 const InviteCodePage = async ({
     params
 }: inviteCodePageProps) => {
+    const { inviteCode } = params;
     const profile = await currentProfile()
 
     if (!profile) {
         return redirect('/sign-in')
     }
 
-    if (!params.inviteCode) {
+    if (!inviteCode) {
         return redirect('/')
     }
 
     const existingServer = await db.server.findFirst({
         where: {
-            inviteCode: params.inviteCode,
+            inviteCode: inviteCode,
             members: {
                 some: {
                     profileId: profile.id
@@ -38,7 +101,7 @@ const InviteCodePage = async ({
 
     const server = await db.server.update({
         where: {
-            inviteCode: params.inviteCode
+            inviteCode: inviteCode
         },
         data: {
             members: {
