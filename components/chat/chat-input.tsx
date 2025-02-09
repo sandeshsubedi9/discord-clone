499ee93dcded24e77e31d5ae496cbb07c720a166
Sandesh <sandeshsubedi2020@gmail.com>
2025-02-09 15:17:09 +0545

    fix dependencies issue

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -7,7 +7,7 @@ import axios from 'axios'
 import qs from 'query-string'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Form, FormControl, FormField, FormItem } from '../ui/form'
-import { Plus, } from 'lucide-react'
+import { Plus, Send } from 'lucide-react'
 import { Input } from '../ui/input'
 import { useModal } from '@/hooks/use-modal-store'
 import EmojiPicker from '../emoji-picker'
@@ -42,21 +42,23 @@ const ChatInput = ({
 
     const isLoading = form.formState.isSubmitting
 
-    const onSubmit = async (values: z.infer<typeof formSchema>)=>{
-       try{
-        const url = qs.stringifyUrl({
-            url: apiUrl,
-            query
-        })
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            const url = qs.stringifyUrl({
+                url: apiUrl,
+                query
+            })
 
-        await axios.post(url, values)
-        form.reset()
-        router.refresh()
-       } 
-       catch (error) {
-        console.log(error)
-       }
+            await axios.post(url, values)
+            form.reset()
+            router.refresh()
+        }
+        catch (error) {
+            console.log(error)
+        }
     }
+
+    const messageContent = form.watch("content", "");
 
     return (
         <Form {...form}>
@@ -68,21 +70,26 @@ const ChatInput = ({
                         <FormItem>
                             <FormControl>
                                 <div className='relative p-4 pb-6'>
-                                    <button 
-                                    onClick={()=>{onOpen('messageFile', {apiUrl, query})}}
-                                    type='button' className='absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-600 transition rounded-full p-1 flex items-center justify-center'>
-                                        <Plus className='text-white dark:text-[#313338]'/>
+                                    <button
+                                        onClick={() => { onOpen('messageFile', { apiUrl, query }) }}
+                                        type='button' className='absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-600 transition rounded-full p-1 flex items-center justify-center'>
+                                        <Plus className='text-white dark:text-[#313338]' />
                                     </button>
                                     <Input
-                                    disabled={isLoading}
-                                    className='px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus:outline-none focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200 '
-                                    placeholder={`Message ${type==='conversation' ? name: "#"+name}` }
-                                    {...field}
+                                        disabled={isLoading}
+                                        className='px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus:outline-none focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200 '
+                                        placeholder={`Message ${type === 'conversation' ? name : "#" + name}`}
+                                        {...field}
                                     />
-                                    <div className='absolute top-7 right-8'>
+                                    <div className='flex gap-3 absolute top-7 right-8'>
+
                                         <EmojiPicker
-                                        onChange={(emoji:string)=>field.onChange(`${field.value} ${emoji}`)} 
+                                            onChange={(emoji: string) => field.onChange(`${field.value} ${emoji}`)}
                                         />
+                                        <button type='submit' disabled={!messageContent.trim() || isLoading}>
+                                            <Send className={`transition  ${messageContent.trim() ? "dark:text-zinc-200 text-zinc-600" : "text-zinc-400"
+                                                }`} />
+                                        </button>
                                     </div>
                                 </div>
                             </FormControl>
@@ -90,8 +97,8 @@ const ChatInput = ({
                     )}
                 />
             </form>
-        </Form>
-    )
+        </Form >
+    );
 }
 
 export default ChatInput
